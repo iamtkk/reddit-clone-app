3903b793bdcd9b3426da8b383584c5e73f355f7c
iamtkk <iamtkk2@gmail.com>
2023-07-31 14:07:20 +0900

    typeorm

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,5 +1,14 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+// import * as glob from "glob";
+import Sub from "./entities/Sub";
+import Post from "./entities/Post";
+import Vote from "./entities/Vote";
+import Comment from "./entities/Comment";
+import Entity from "./entities/Entity";
+import { User } from "./entities/User";
+
+// const entityFiles = glob.sync("src/entities/**/*.ts", { cwd: __dirname });
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -7,10 +16,12 @@ export const AppDataSource = new DataSource({
   port: 5432,
   username: "postgres",
   password: "password",
-  database: "postgres",
+  database: "reddit",
   synchronize: true,
   logging: false,
-  entities: ["src/entities/**/*.ts"],
+  // entities: ["src/entities/**/*.ts"],
+  // entities: entityFiles,
+  entities: [Sub, User, Post, Vote, Comment, Entity],
   migrations: [],
   subscribers: [],
 });
