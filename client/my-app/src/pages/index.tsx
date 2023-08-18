c59343fab3d97b5b1613e1570275e9bc0f160331
iamtkk <iamtkk2@gmail.com>
2023-08-19 08:52:50 +0900

    SWR

--- client/my-app/src/pages/index.tsx
+++ client/my-app/src/pages/index.tsx
@@ -1,11 +1,32 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
 // import styles from '@/styles/Home.module.css';
-import register from './register';
+import register from "./register";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex max-w-5xl px-4 pt-5 mx-auto">
+      <div className="w-full md:mr-3 md:w-8/12"></div>
+      {/* 사이드바 */}
+      <div className="hidden w-4/12 ml-3 md:block">
+        <div className="bg-white border rounded">
+          <div className="p-4 border-b">
+            <p className="text-lg font-semibold text-center">상위 커뮤니티</p>
+          </div>
+          {/* 커뮤니티 리스트 */}
+          <div className="w-full py-6 text-center">
+            <Link href="/subs/create" legacyBehavior>
+              <a className="w-full p-2 text-center text-white bg-gray-400 rounded">
+                커뮤니티 만들기
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/my-app/src/types.ts
+++ client/my-app/src/types.ts
@@ -4,3 +4,13 @@ export interface User {
   createdAt: string;
   updatedAt: string;
 }
+
+export interface Sub {
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  title: string;
+  description: string;
+  imageUrn: string;
+  bannerUrn: string;
+}
